1bf6614c86d903da7aace7b93b0168363cd92d91
Zhuojin Li <zhuojinl@usc.edu>
2020-12-02 16:17:37 -0800

    Fix a tracer bug in server

--- distbelief/server.py
+++ distbelief/server.py
@@ -82,37 +82,36 @@ class ParameterServer:
                 # Receive gradients in the reverse order
                 for buffer in reversed(gradient_buffers):
                     receivers.append(dist.irecv(tensor=buffer, src=server_id - 1))
-                else:
-                    for i in range(len(gradient_buffers)-1, -1, -1):
-                        with tracer.start_active_span('recv') as span:
-                            span.set_tag('layer', layer_name[i][0])
-                            span.set_tag('type', layer_name[i][1])
-                            receivers[i].wait()
-                    with tracer.start_active_span('update'):
-                        for i, gradients in enumerate(gradient_buffers):
-                            if cuda:
-                                global_model[i].add_(gradients.cuda())
-                            else:
-                                global_model[i].add_(gradients)
-                    tensor = torch.zeros(1)
-                    dist.recv(tensor=tensor, src=server_id - 1)
-                    span_step.finish()
-                    if tensor[0] == float('inf'):
-                        break
-                    span_step = tracer.start_span('step {}'.format(step_num))
-                    step_num += 1
-                    with tracer.start_active_span('collect'):
-                        for i, gradients in enumerate(gradient_buffers):
-                            if cuda:
-                                gradient_buffers[i] = global_model[i].cpu()
-                            else:
-                                gradients.copy_(global_model[i])
-                    for i, para in enumerate(gradient_buffers):
-                        with tracer.start_active_span('send') as span:
-                            span.set_tag('size', para.nelement() * para.element_size())
-                            span.set_tag('layer', layer_name[i][0])
-                            span.set_tag('type', layer_name[i][1])
-                            dist.send(para, dst=server_id - 1)
+                for i, receiver in enumerate(receivers):
+                    with tracer.start_active_span('recv') as span:
+                        span.set_tag('layer', layer_name[-1 - i][0])
+                        span.set_tag('type', layer_name[-1 - i][1])
+                        receiver.wait()
+                with tracer.start_active_span('update'):
+                    for i, gradients in enumerate(gradient_buffers):
+                        if cuda:
+                            global_model[i].add_(gradients.cuda())
+                        else:
+                            global_model[i].add_(gradients)
+                tensor = torch.zeros(1)
+                dist.recv(tensor=tensor, src=server_id - 1)
+                span_step.finish()
+                if tensor[0] == float('inf'):
+                    break
+                span_step = tracer.start_span('step {}'.format(step_num))
+                step_num += 1
+                with tracer.start_active_span('collect'):
+                    for i, gradients in enumerate(gradient_buffers):
+                        if cuda:
+                            gradient_buffers[i] = global_model[i].cpu()
+                        else:
+                            gradients.copy_(global_model[i])
+                for i, para in enumerate(gradient_buffers):
+                    with tracer.start_active_span('send') as span:
+                        span.set_tag('size', para.nelement() * para.element_size())
+                        span.set_tag('layer', layer_name[i][0])
+                        span.set_tag('type', layer_name[i][1])
+                        dist.send(para, dst=server_id - 1)
         dist.destroy_process_group()
         tracer.export_traces("server{}.json".format(server_id))
         print("server {} finished".format(server_id))
